6ef8b44e4f94fdd6e650657ee51b8b99e477c602
[magdalena almanova] <[magdaalmanova@gmail.com]>
2019-12-08 16:20:36 +0100

    random numbers

--- models.py
+++ models.py
@@ -1,9 +1,11 @@
 from app import db
+from sqlalchemy import Table, Column, Integer, String, MetaData
 
-class Pernicky(db.Model):
+class Pernicky1(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.DateTime, index=True, unique=True)
     uloha = db.Column(db.String(250), index=True, unique=True)
+    url = db.Column(db.String(120), index=True, unique=True)
 
    
   
