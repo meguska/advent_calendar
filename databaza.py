1bffcc9ee3af415d4c688bfd79e132551dbb7ce1
[magdalena almanova] <[magdaalmanova@gmail.com]>
2019-12-11 21:42:42 +0100

    databaza

--- databaza.py
+++ databaza.py
@@ -5,4 +5,5 @@ pernicky1= pandas.read_csv('pernicky_data.csv', encoding='utf-8')
 colu= pernicky1.columns
 
 neviem= pernicky1[['ID', 'Aktivita']]
-print(neviem)
+data = pernicky1.values.tolist()
+print(data)
